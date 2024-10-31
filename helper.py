df6463549a8e51f670247bb64e76af77a8755f49
johnvo24 <vochidung243@gmail.com>
2024-10-31 22:48:38 +0700

    John updated

--- helper.py
+++ helper.py
@@ -4,15 +4,27 @@ class Helper():
 # Time
   def seperate_time(self, time_str):
     dt = pd.to_datetime(time_str)
-    return [dt.day_of_year, dt.day, dt.year, dt.month, dt.date, ]
+    return [dt.year*100000+dt.day_of_year*100+dt.hour, dt.day_of_week, dt.year, dt.month, dt.day, dt.hour]
 
-
-  def encode_time(self, filepath="", column="time"):
+  def encode_time(self, filepath="", column="time", save_to=None):
     df = pd.read_csv(filepath)[:100]
-    dt = 
-    df[column] = df[column].apply(lambda x: )
-    print(df.head(10))
-    # return df
+    data = [self.seperate_time(time_str) for time_str in df[column]]
+    df_time = pd.DataFrame(data, columns=["time_id", "day", "year", "month", "date", "hour"])
+    if(save_to): df_time.to_csv(save_to, index=False)
+    df[column] = df_time["time_id"]
+    # print(df.head(10))
+    df.to_csv(filepath, index=False)
+    return df_time
 
+# Concat
+  def concat_csv(self, filepaths, save_to=None):
+    dfs = [pd.read_csv(filepath) for filepath in filepaths]
+    df_concat = pd.concat(dfs)
+    if(save_to): df_concat.to_csv(save_to, index=False)
+    return df_concat
 
-Helper.encode_time(filepath="data/open-meteo-21.00N105.88E19m.csv")
+# Helper().encode_time("data/open-meteo-21.00N105.88E19m.csv", save_to="data/dim_time/dim_time.csv")
+# Helper().concat_csv(filepaths=[
+#   "data/dim_time/dim_time_1.csv",
+#   "data/dim_time/dim_time_2.csv",
+# ], save_to="data/dim_time/dim_time.csv")
